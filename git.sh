76dd778353e3e7ab7e5abde5487b1c7edf172ec2
Dr Nic Williams <drnicwilliams@gmail.com>
2010-09-26 15:44:26 -0700

    tests if git or heroku is installed before executing their modules

--- git.sh
+++ git.sh
@@ -1,24 +1,27 @@
-alias gst='git status'
-alias gl='git pull'
-alias gp='git push'
-alias gpa='git push-all' # see [alias] in ~/.gitconfig
-alias gd='git diff | mate'
-alias ga='git add'
-alias gcl='git config --list'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gbc='git branch --color'
-alias gba='git branch -a'
-alias gco='git checkout'
-alias gpatch='git diff master -p'
-alias gitrm="git status | grep deleted | awk '{print $3}' | xargs git rm"
-alias gitx="open -b nl.frim.GitX"
+if [ -n `which git` ]; then
 
-function gpall() {
-  for remote in `git remote show`
-  do
-    `git push $remote --all`
-  done
-}
+  alias gst='git status'
+  alias gl='git pull'
+  alias gp='git push'
+  alias gpa='git push-all' # see [alias] in ~/.gitconfig
+  alias gd='git diff | mate'
+  alias ga='git add'
+  alias gcl='git config --list'
+  alias gc='git commit -v'
+  alias gca='git commit -v -a'
+  alias gb='git branch'
+  alias gbc='git branch --color'
+  alias gba='git branch -a'
+  alias gco='git checkout'
+  alias gpatch='git diff master -p'
+  alias gitrm="git status | grep deleted | awk '{print $3}' | xargs git rm"
+  alias gitx="open -b nl.frim.GitX"
 
+  function gpall() {
+    for remote in `git remote show`
+    do
+      `git push $remote --all`
+    done
+  }
+
+fi
--- heroku.sh
+++ heroku.sh
@@ -1,9 +1,13 @@
-alias gph='git push heroku HEAD:master'
-alias h='heroku'
+if [ -n `which heroku` ]; then
 
-function heroku_transfer {
-  heroku sharing:add $1
-  heroku sharing:transfer $1
-  git config --add heroku.email $1
-  git config --add heroku.password "$2"
-}
+  alias gph='git push heroku HEAD:master'
+  alias h='heroku'
+
+  function heroku_transfer {
+    heroku sharing:add $1
+    heroku sharing:transfer $1
+    git config --add heroku.email $1
+    git config --add heroku.password "$2"
+  }
+
+fi
