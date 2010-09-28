c79a4689a6919d848d2b560719eb2539a9319110
Dr Nic Williams <drnicwilliams@gmail.com>
2010-09-28 05:53:41 -0700

    allow 9 arguments to rails script commands

--- rails.sh
+++ rails.sh
@@ -1,9 +1,9 @@
 function sc {
   if [ -x script/console ]; then
-    script/console $1
+    script/console $1 $2 $3 $4 $5 $6 $7 $8 $9
   else
     if [ -x script/rails ]; then
-      script/rails c $1
+      script/rails c $1 $2 $3 $4 $5 $6 $7 $8 $9
     else
       irb -rubygems -rmap_by_method $1
     fi
@@ -12,20 +12,20 @@ function sc {
 
 function gen {
   if [ -x script/generate ]; then
-    script/generate $1
+    script/generate $1 $2 $3 $4 $5 $6 $7 $8 $9
   else
     if [ -x script/rails ]; then
-      script/rails g $1
+      script/rails g $1 $2 $3 $4 $5 $6 $7 $8 $9
     fi
   fi
 }
 
 function destroy {
   if [ -x script/generate ]; then
-    script/destroy $1
+    script/destroy $1 $2 $3 $4 $5 $6 $7 $8 $9
   else
     if [ -x script/rails ]; then
-      script/rails destroy $1
+      script/rails destroy $1 $2 $3 $4 $5 $6 $7 $8 $9
     fi
   fi
 }
