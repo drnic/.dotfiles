0a5f969b3628a874f097a3290484ce23136d069d
Dr Nic Williams <drnicwilliams@gmail.com>
2011-05-03 10:19:50 -0700

    Specific 'm' functions for different editors

--- editor.sh
+++ editor.sh
@@ -2,34 +2,49 @@ function use_textmate () {
   export EDITOR="/usr/local/bin/mate -w"
   export GEM_OPEN_EDITOR="/usr/local/bin/mate"
   alias tm_dialog=~/"Library/Application\ Support/TextMate/PlugIns/Dialog.tmplugin/Contents/Resources/tm_dialog"
+
+  function m {
+    if [ "$1" != "" ]; then
+      mate $1
+    else
+      mate .
+    fi
+  }
 }
 
 function use_redcar_gem () {
-  export EDITOR="rvm jruby exec redcar -w"
-  export GEM_OPEN_EDITOR="redcar"
+  use_redcar
 }
 
 function use_redcar_dev () {
   export PATH="/Users/drnic/Projects/ruby_apps/redcar/bin:$PATH"
-  export EDITOR="rvm jruby exec redcar -w"
-  export GEM_OPEN_EDITOR="redcar"
+  use_redcar
 }
+
+function use_redcar () {
+  export EDITOR="rvm jruby exec redcar --no-splash -w"
+  export GEM_OPEN_EDITOR="redcar"
+
+  function m {
+    if [ "$1" != "" ]; then
+      rvm jruby exec redcar --no-splash $1
+    else
+      rvm jruby exec redcar --no-splash .
+    fi
+  }
+
+}
+
+
+use_textmate
 
 # don't try to run redcar if jruby not installed
-if rvm list strings | grep -q jruby
-then
-  use_redcar_dev
-else
-  use_textmate
-fi
-
-function m {
-  if [ "$1" != "" ]; then
-    $EDITOR $1
-  else
-    $EDITOR .
-  fi
-}
+# if rvm list strings | grep -q jruby
+# then
+#   use_redcar_dev
+# else
+#   use_textmate
+# fi
 
 alias et="m" # my brain still likes to type 'et' to get a text editor
 
