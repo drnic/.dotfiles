c6a351adcb03ec1ed33e10b6bf1f953be364bef2
Dr Nic Williams <drnicwilliams@gmail.com>
2011-08-23 07:38:57 -0700

    Look for node bins in user folder

--- node.sh
+++ node.sh
@@ -1,2 +1,4 @@
 export PATH=$PATH":/usr/local/share/npm/bin"
+export PATH=$PATH":~/node_modules/.bin"
 export NODE_PATH=$NODE_PATH":/usr/local/lib/node"
+export NODE_PATH=$NODE_PATH":/usr/local/lib/node_modules"
