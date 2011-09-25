44a33bfa55f154bd8ed7771c9229181765a84835
Dr Nic Williams <drnicwilliams@gmail.com>
2011-09-25 10:35:00 -0700

    using ruby 1.9.3 by default now

--- ruby.sh
+++ ruby.sh
@@ -7,6 +7,6 @@ function glg() {
 alias be="bundle exec"
 
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
-rvm 1.9.2-p180
+rvm ruby-1.9.3
 # rvm gemset use 1.9.2-p180@global
 echo "Using "`ruby -v`
